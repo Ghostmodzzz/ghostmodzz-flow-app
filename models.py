c9edcb346b72f353b8ce382dbf123e979af9ac04
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-07 21:59:13 -0400

    Update models.py

--- models.py
+++ models.py
@@ -2,21 +2,21 @@ from extensions import db
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    bills = db.relationship('Bill', backref='user', lazy=True)
+    id        = db.Column(db.Integer, primary_key=True)
+    email     = db.Column(db.String(255), unique=True, nullable=False)
+    password  = db.Column(db.String(255), nullable=False)
+    bills     = db.relationship('Bill', backref='user', lazy=True)
     paychecks = db.relationship('Paycheck', backref='user', lazy=True)
 
 class Bill(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    amount = db.Column(db.Numeric(10,2), nullable=False)
+    id       = db.Column(db.Integer, primary_key=True)
+    name     = db.Column(db.String(255), nullable=False)
+    amount   = db.Column(db.Numeric(10,2), nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id  = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class Paycheck(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, nullable=False)
-    amount = db.Column(db.Numeric(10,2), nullable=False)
+    id      = db.Column(db.Integer, primary_key=True)
+    date    = db.Column(db.Date, nullable=False)
+    amount  = db.Column(db.Numeric(10,2), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
