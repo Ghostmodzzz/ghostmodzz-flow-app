5f88fda9a907dfd74ae0ec9d4a503a051f7b3ef9
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-07 20:08:38 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,67 +1,18 @@
-
-from flask import Flask, render_template, redirect, url_for, flash, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from forms import RegistrationForm, LoginForm
-from models import User
-import os
+from flask import Flask
+from flask_login import LoginManager
+from extensions import db
+from config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thisshouldbeasecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db.init_app(app)
+
+login_manager = LoginManager()
 login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+from routes import *
 
-@app.route("/")
-def home():
-    return "GhostModzz Flow - It works!"
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You can now log in.', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            return redirect(url_for('dashboard'))
-        else:
-            flash('Login Unsuccessful. Please check email and password.', 'danger')
-    return render_template('login.html', title='Login', form=form)
-
-@app.route("/dashboard")
-@login_required
-def dashboard():
-    return render_template('dashboard.html', title='Dashboard')
-
-@app.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
