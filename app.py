8036b45ff904f02348fad5df09de9585f4b6e1b4
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-08 01:19:04 -0400

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-# app.py
-from factory import create_app
-
-# Create the app instance
-app = create_app()
-
-# Only run the app if this file is executed directly
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
