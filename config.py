928581cb72adb3ab1d34d6d40aaec2765dd7e602
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-08 02:16:53 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,12 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # SendGrid Email Setup
     SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
-    OPENROUTER_API_KEY = os.environ.get('OPENROUTER_API_KEY')
     EMAIL_SENDER = os.environ.get('EMAIL_SENDER')
+
+    # OpenRouter AI API Key
+    OPENROUTER_API_KEY = os.environ.get('OPENROUTER_API_KEY')
