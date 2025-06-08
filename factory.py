56f87c49649a10b639890baa83a8197c6a3b1631
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-08 01:43:28 -0400

    Update factory.py

--- factory.py
+++ factory.py
@@ -1,11 +1,20 @@
 from flask import Flask
-from extensions import db, login_manager
-from routes import main
+from config import Config
+from extensions import db, login_manager, mail
+from routes import main as main_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config.Config')
+    app.config.from_object(Config)
+
+    # Initialize Extensions
     db.init_app(app)
     login_manager.init_app(app)
-    app.register_blueprint(main)
+    mail.init_app(app)
+
+    login_manager.login_view = 'main.login'
+
+    # Register Blueprints
+    app.register_blueprint(main_blueprint)
+
     return app
