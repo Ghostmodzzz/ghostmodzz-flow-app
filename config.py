09dd6141f1f199d12e5a83e97bf9a048a41ad55b
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-07 23:43:59 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
 import os
 
 class Config:
-    SECRET_KEY               = os.environ["SECRET_KEY"]
-    SECURITY_PASSWORD_SALT   = os.environ["SECURITY_PASSWORD_SALT"]
+    # Flask & WTF
+    SECRET_KEY               = os.environ.get("SECRET_KEY")
+    SECURITY_PASSWORD_SALT   = os.environ.get("SECURITY_PASSWORD_SALT")
 
+    # Database (if you’re using Render Postgres, you can also pull DATABASE_URL here)
     SQLALCHEMY_DATABASE_URI  = os.environ.get("DATABASE_URL", "sqlite:///site.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SENDGRID_API_KEY         = os.environ["SENDGRID_API_KEY"]
-    OPENROUTER_API_KEY       = os.environ["OPENROUTER_API_KEY"]
+    # SendGrid
+    SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+    EMAIL_SENDER     = os.environ.get("EMAIL_SENDER")
+
+    # OpenRouter
+    OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY")
