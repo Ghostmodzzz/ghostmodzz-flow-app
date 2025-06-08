91fe809bfc3011e603e876c5018272756a2b2e4a
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-08 01:21:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-# Flask app entry point
+# app.py
+from factory import create_app
+
+# Create the Flask app instance
+app = create_app()
+
+if __name__ == "__main__":
+    # For local development only
+    app.run(host='0.0.0.0', port=5000, debug=True)
