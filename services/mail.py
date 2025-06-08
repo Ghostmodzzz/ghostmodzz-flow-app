c154cfff0e2b03063ae4f3bc49f3896d529ff269
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-08 00:09:04 -0400

    Update mail.py

--- services/mail.py
+++ services/mail.py
@@ -1,23 +1,25 @@
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
-
 from config import Config
 
-def send_confirmation_email(to_email: str, token: str):
-    """
-    Send the account‐confirmation email via SendGrid.
-    """
-    confirm_url = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/confirm/{token}"
+def send_confirmation_email(to_email, token):
+    confirm_url = f"https://ghostmodzz-flow-app.onrender.com/confirm/{token}"
+    
     message = Mail(
         from_email=Config.EMAIL_SENDER,
         to_emails=to_email,
-        subject="Please confirm your GhostModzz Flow email",
+        subject="Confirm Your Email",
         html_content=f"""
-            <p>Welcome to GhostModzz Flow!</p>
-            <p>Click <a href="{confirm_url}">here to confirm your email</a>.</p>
-            <p>This link will expire in 1 hour.</p>
+        <p>Welcome to GhostModzz Flow!</p>
+        <p>Please confirm your email by clicking on the link below:</p>
+        <a href="{confirm_url}">Confirm your email</a>
+        <p>If you did not request this, please ignore this email.</p>
         """
     )
-    client = SendGridAPIClient(api_key=Config.SENDGRID_API_KEY)
-    client.send(message)
+    
+    try:
+        sg = SendGridAPIClient(Config.SENDGRID_API_KEY)
+        sg.send(message)
+    except Exception as e:
+        print(f"Error sending email: {e}")
