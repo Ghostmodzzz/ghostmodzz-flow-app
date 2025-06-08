a69c6a014e46d5fec69ec31f6a21c881828b261b
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-08 03:11:09 -0400

    Update factory.py

--- factory.py
+++ factory.py
@@ -1,21 +1,13 @@
-from flask import Flask
-from extensions import db, login_manager, mail
-from config import Config
+from flask_migrate import Migrate
+
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-
-    # Auto-create database tables
-    with app.app_context():
-        from models import User, Bill, Paycheck
-        db.create_all()
-
-    from routes import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
+    migrate.init_app(app, db)  # <- Add this
+    ...
     return app
