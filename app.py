8ce5fae65bb4d1b65fdcd0f690930d6c2b516f52
Ghostmodzzz <t_neal21511@outlook.com>
2025-06-07 20:29:10 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,60 @@
-from flask import Flask
-from flask_login import LoginManager
-from extensions import db
+
+from flask import Flask, render_template, redirect, url_for, flash, request
 from config import Config
+from forms import RegistrationForm, LoginForm
+from models import User
+from extensions import db, bcrypt, login_manager
+from flask_login import login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
-
-login_manager = LoginManager()
-login_manager.login_view = 'login'
+bcrypt.init_app(app)
 login_manager.init_app(app)
 
-from routes import *
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/")
+def home():
+    return redirect(url_for('login'))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Login Unsuccessful. Please check email and password", "danger")
+    return render_template("login.html", form=form)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created successfully!", "success")
+        return redirect(url_for('login'))
+    return render_template("register.html", form=form)
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html", name=current_user.email)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
